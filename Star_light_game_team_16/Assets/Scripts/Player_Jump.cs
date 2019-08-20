c10b6d34c963fec6b4dd066b8d11618342e1a0fb
Ryan Sheridan <ryansheridan555@gmail.com>
2019-08-20 19:27:52 +1000

    Mini-game (work in progress)
    Mini-game that Ben and I are working on.

---
+++ Star_light_game_team_16/Assets/Scripts/Player_Jump.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Jump : MonoBehaviour
+
+{
+    public float downTime, upTime = 0;    
+    public bool ready = false;
+    private bool moving = false;
+    public float timeCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+
+        Jump();
+        if (moving == true)
+        {            
+            transform.position += Vector3.up/6;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && (downTime - timeCheck) > 0)
+        {
+            moving = true;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && moving != true)
+        {            
+            downTime = Time.time;         
+            
+        }
+        else if (moving != true)
+        {
+            timeCheck = Time.time;
+        }
+                
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    
+    {        
+        if (other.gameObject.tag == "Level1" && ((downTime - timeCheck) >= 0 && (downTime - timeCheck) < 1))
+        {
+            moving = false;
+        }
+
+        if (other.gameObject.tag == "Level2" && ((downTime - timeCheck) >= 1 && (downTime - timeCheck) < 2))
+        {
+            moving = false;
+        }
+
+        if (other.gameObject.tag == "Level3" && ((downTime - timeCheck) >= 2))
+        {
+            moving = false;
+        }
+    }
+}
