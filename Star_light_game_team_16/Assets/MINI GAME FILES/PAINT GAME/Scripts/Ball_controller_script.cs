9930166c87bbd9717af193cef50a58f80945fbc1
Mebradhen <mrbardhen@gmail.com>
2019-08-23 21:12:05 +1000

    paint game

---
+++ Star_light_game_team_16/Assets/MINI GAME FILES/PAINT GAME/Scripts/Ball_controller_script.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [RequireComponent(typeof(Rigidbody2D))]
+public class Ball_controller_script : MonoBehaviour
+{
+    public float speed = 90;
+
+    private Rigidbody2D rb;
+    private Vector2 movementVector;
+
+    public GameObject paint_smoge;
+
+    private float paint_wait = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movementVector = Vector2.zero;
+    }
+
+    void Update()
+    {
+
+        if (transform.position.x > 8.0f)
+        {
+            movementVector.x = -1;
+        }
+        else if(transform.position.x < -8.0f)
+        {
+            movementVector.x = 1;
+        }
+        else
+        {
+            movementVector.x = Input.GetAxis("Horizontal");
+        }
+
+
+        if (transform.position.y > 6.0f)
+        {
+            movementVector.y = -1;
+        }
+        else if (transform.position.y < -2.0f)
+        {
+            movementVector.y = 1;
+        }
+        else
+        {
+            movementVector.y = Input.GetAxis("Vertical");
+        }
+
+        paint_wait += 15.0f * Time.deltaTime;
+
+        rb.velocity = movementVector * speed * Time.deltaTime;
+
+        if ((movementVector.x > 0.3f || movementVector.x < -0.3f) || (movementVector.y > 0.3f || movementVector.y < -0.3f))
+        { 
+            if (paint_wait > 1.0f)
+            {
+
+                Instantiate(paint_smoge, transform.position, transform.rotation);
+
+                paint_wait = 0.00f;
+            }
+         }
+
+ 
+    }
+}
---
+++ Star_light_game_team_16/Assets/MINI GAME FILES/PAINT GAME/Scripts/mini_game_paint_detc.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mini_game_paint_detc : MonoBehaviour
+{
+
+    private int the_check = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Ball")
+        {
+            if(the_check == 0)
+            {
+
+
+                the_check = 1;
+
+                GameObject go = GameObject.Find("Object_detection");
+
+                main_count cs = go.GetComponent<main_count>();
+
+                cs.count += 1; 
+
+            }
+        }
+    }
+}
