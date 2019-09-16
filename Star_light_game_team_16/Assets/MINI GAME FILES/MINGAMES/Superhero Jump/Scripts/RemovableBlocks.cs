47c5fb5967756f643b6c837edf96bda7f13c3247
Ryan Sheridan <ryansheridan555@gmail.com>
2019-09-17 02:11:45 +1000

    Superhero Jump 3
    difficulty level 3

---
+++ Star_light_game_team_16/Assets/MINI GAME FILES/MINGAMES/Superhero Jump/Scripts/RemovableBlocks.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovableBlocks : MonoBehaviour
+{
+    public float timeSpent;
+    public float timeSubtraction;
+    public float timeActive = 0f;
+    public float timeNotActive = 0f;
+    public GameObject gameObjects;
+    public bool active = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("removable");        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active == true)
+        {
+            timeActive = Time.time;
+        }
+
+        if (active == false)
+        {
+            timeNotActive = Time.time;
+        }
+
+
+        if (Time.time - timeNotActive >= 2f)
+        {
+            gameObjects.SetActive(false);
+            active = false;
+        }
+
+        if (Time.time - timeActive >= 2f)
+        {
+            gameObjects.SetActive(true);
+            active = true;
+        }
+    }
+}
