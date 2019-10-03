63c44a36bed5623ceaed7c3b9392205d379e48ab
Ahamkaraz <53844569+Ahamkaraz@users.noreply.github.com>
2019-10-03 14:09:58 +1000

    slide ride games implemented
    slide ride games implemented

---
+++ Star_light_game_team_16/Assets/MINI GAME FILES/MINGAMES/SLIDE GAME/SLIDE GAME/level1player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level1player : MonoBehaviour
+
+{
+    private float speed = 2f;
+    private Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetComponent<Rigidbody>().velocity = transform.up;
+
+        if (Input.GetKeyDown(KeyCode.Q) && transform.position == new Vector3(transform.position.x, transform.position.y, 2))
+        {
+           // transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
